52aa4e2d377ea5ef211466e8643a9577ab35c497
CaioBreis <caiobispor@gmail.com>
2025-05-29 19:17:32 -0300

    fdsf

--- TodoApp-SOLID-UPDATED/TodoApp.java
+++ TodoApp-SOLID-UPDATED/TodoApp.java
@@ -5,6 +5,7 @@ import todoapp.ui.ConsoleUI;
 public class TodoApp {
     public static void main(String[] args) {
         System.out.println("L I S T A    D E    T A R E F A S");
+        System.out.println("L I S T A    D E    T A R E F A S");
         ConsoleUI ui = new ConsoleUI();
         ui.start();
     }
